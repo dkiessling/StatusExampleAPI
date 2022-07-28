c1b8f79306a63b4d1857f33b2b537e678ddb4127
Daniel Kiessling <dkiessling@mailbox.org>
2022-07-28 13:35:52 +0200

    changed to non-mimimal structure

--- StatusExampleAPI/Program.cs
+++ StatusExampleAPI/Program.cs
@@ -1,54 +1,60 @@
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+internal class Program
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Status Example API",
-        Description = "An ASP.NET Core Web API for testing HTTP status codes",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Example Contact",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Example License",
-            Url = new Uri("https://example.com/license")
-        }
-    });
+	private static void Main( string[] args )
+	{
+		var builder = WebApplication.CreateBuilder( args );
 
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+		// Add services to the container.
 
-var app = builder.Build();
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen( options =>
+		{
+			options.SwaggerDoc( "v1", new OpenApiInfo
+			{
+				Version = "v1",
+				Title = "Status Example API",
+				Description = "An ASP.NET Core Web API for testing HTTP status codes",
+				TermsOfService = new Uri( "https://example.com/terms" ),
+				Contact = new OpenApiContact
+				{
+					Name = "Example Contact",
+					Url = new Uri( "https://example.com/contact" )
+				},
+				License = new OpenApiLicense
+				{
+					Name = "Example License",
+					Url = new Uri( "https://example.com/license" )
+				}
+			} );
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = "docs";
-    });
+			var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+			options.IncludeXmlComments( Path.Combine( AppContext.BaseDirectory, xmlFilename ) );
+		} );
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if ( app.Environment.IsDevelopment() )
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI( options =>
+			{
+				options.SwaggerEndpoint( "/swagger/v1/swagger.json", "v1" );
+				options.RoutePrefix = "docs";
+			} );
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
